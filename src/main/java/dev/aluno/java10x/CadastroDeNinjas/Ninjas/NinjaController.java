fe4843fad31abd4c908752e2dce086c8e71c751f
Diogo Sabec <diogovanzosabec@gmail.com>
2025-02-04 21:04:02 -0300

    feat: NinjaController updated

--- src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.aluno.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,36 @@ public class NinjaController {
     public String boasVindas(){
         return "Olá Mundo!";
     }
+
+    // CRUD
+    // Add ninja (Create)
+    @PostMapping("/create")
+    public String createNinja() {
+        return "Ninja Created";
+    }
+
+    // Show all ninjas (Read)
+    @GetMapping("/all")
+    public String showAllNinjas(){
+        return "Show all ninjas";
+    }
+
+    // Show all ninjas by ID (Read)
+    @GetMapping("/allID")
+    public String showNinjaByID(){
+        return "Show all ninjas by ID";
+    }
+    // Update Ninja by ID (Update)
+    @PutMapping("/updateID")
+    public String updateNinja(){
+        return "Update Ninja by ID";
+    }
+
+    // Delete Ninja (Delete)
+    @DeleteMapping("/deleteID")
+    public String deleteNinjaByID(){
+        return "Delete ninja by ID";
+    }
+
+
 }
