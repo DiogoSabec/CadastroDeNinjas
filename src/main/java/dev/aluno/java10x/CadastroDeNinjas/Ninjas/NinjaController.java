5c0c20b1274e81df7bf48843fffd64648effff5d
Diogo Sabec <diogovanzosabec@gmail.com>
2025-02-04 22:05:56 -0300

    feat: Shows all ninjas using JPA

--- src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,9 +2,17 @@ package dev.aluno.java10x.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninja")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
@@ -20,8 +28,8 @@ public class NinjaController {
 
     // Show all ninjas (Read)
     @GetMapping("/all")
-    public String showAllNinjas(){
-        return "Show all ninjas";
+    public List<NinjaModel> showAllNinjas(){
+        return ninjaService.showAllNinjas();
     }
 
     // Show all ninjas by ID (Read)
@@ -40,6 +48,5 @@ public class NinjaController {
     public String deleteNinjaByID(){
         return "Delete ninja by ID";
     }
-
 
 }
