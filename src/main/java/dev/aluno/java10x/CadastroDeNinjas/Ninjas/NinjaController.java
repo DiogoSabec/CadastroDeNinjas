8facd3add9a02c6dfb5ec38420dc1bac0c5a59cf
Diogo Sabec <diogovanzosabec@gmail.com>
2025-02-04 21:26:55 -0300

    fix: added @requestMapping to NinjaController

--- src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package dev.aluno.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninja")
 public class NinjaController {
 
     @GetMapping("/boasVindas")
